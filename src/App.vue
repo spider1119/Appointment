1e628388d44753498fb3c90e4ceb728b9adc82c4
spider1119 <spider19981119@outlook.com>
2022-02-10 00:42:58 +0800

    first step

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>Appointment Management System</h1>
+    <div id="nav">
+      <router-link :to="{ name: 'home' }">Home</router-link> | 
+      <router-link :to="{ name: 'appointments' }">Appointments</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  #nav {
+    font-size: 20px;
+    color: pink;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <img src="@/assets/logo.png">
+        <h5>Welcome to my web site of Appointment</h5>
+    </div>
+</template>
